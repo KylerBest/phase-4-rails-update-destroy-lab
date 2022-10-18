d4605f15492c95b836f1498844708cf6c88500ed
Kyler Best <kyler.b@live.com>
2022-10-17 22:00:54 -0500

    commit tree 0487d294d70b7f2e177d328ea22c8b13db307b7a

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,12 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    render json: find_plant
+  end
+
+  def update
+    plant = find_plant
+    plant.update(plant_params)
     render json: plant
   end
 
@@ -18,9 +23,20 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
 end
